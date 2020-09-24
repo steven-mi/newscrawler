849eb3f28f1e91ed927c62b75732884041b5de1f
steven-mi <stevenmi@live.de>
2020-09-25 00:45:37 +0200

    fix error in crawler.py

--- newscrawler/crawler.py
+++ newscrawler/crawler.py
@@ -75,7 +75,7 @@ class Crawler:
             self.url = [coerce_url(x) for x in url]
             self.rss_feed = []
             for url in self.url:
-                rss_feeds = extract_rss(url))
+                rss_feeds = extract_rss(url)
                 if not rss_feed:
                     for rss_feed in rss_feeds:
                         self.rss_feed.append(rss_feed)
