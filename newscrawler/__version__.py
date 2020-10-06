5a9a1e3872dec81e8e70c6630be5ea5beea48c40
Steven <stevenmi@live.de>
2020-10-07 01:48:44 +0200

    Update __version__.py

--- newscrawler/__version__.py
+++ newscrawler/__version__.py
@@ -7,7 +7,7 @@ Information about the current version of the NewsCrawler package.
 
 __title__ = 'NewsCrawler3'
 __description__ = 'NewsCrawler - a library to crawl online newspaper articles'
-__version__ = '0.1.6'
+__version__ = '0.1.7'
 __author__ = 'Steven Mi'
 __author_email__ = 's0558366@htw-berlin.de'
 __license__ = 'Apache 2.0'
